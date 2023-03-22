89eedd30236071d08d4ec8c21a6c44bf935c6ad4
korchinmv <korchinmv@yandex.ru>
2023-03-22 17:13:44 +0300

    get api weather

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const API_KEY = "9a95a6d7fbfb4a18b55132621232203";
+const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London`;
+
+const response = fetch(URL)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
